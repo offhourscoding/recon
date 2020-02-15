453c679a454f0f00e9141666288638bf96591dc7
Matt Adams <matt@offhourscoding.com>
2020-02-14 21:25:39 -0500

    fixing install script

--- install.sh
+++ install.sh
@@ -3,16 +3,35 @@
 apt update && apt upgrade -y && apt dist-upgrade -y && apt autoremove -y
 
 apt update
-apt install -y fail2ban openvpn git build-essential python-setuptools golang nmap
+apt install -y fail2ban openvpn git build-essential python-setuptools golang nmap jq python-pip
 
 snap install chromium
 
 mkdir ~/tools
 cd ~/tools
 github.com/michenriksen/aquatone
+
 git clone https://github.com/aboul3la/Sublist3r.git
+cd Sublist3r
+pip install -r requirements.txt
+cd ../
+
 git clone https://github.com/blechschmidt/massdns.git
+cd massdns
+make
+cd ../
+
 git clone https://github.com/robertdavidgraham/masscan.git
+cd masscan
+make
+cd ../
+
 git clone https://github.com/danielmiessler/SecLists.git
 
 go get -u github.com/tomnomnom/httprobe
+export PATH=~/go/bin:$PATH
+echo PATH=~/go/bin:$PATH >> ~/.bashrc
+
+# remove special chars from jhaddix dns wordlist
+cd ~/tools/SecLists/Discovery/DNS/
+cat dns-Jhaddix.txt | head -n -14 > clean-jhaddix-dns.txt
